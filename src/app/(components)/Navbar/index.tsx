40f7f972e025a640812366127f9605ce05c6d0b4
rabeeh_rahman <rabeehrahman.rr@gmail.com>
2024-12-03 00:28:19 +0530

    removed searchbar in navbar and added dark mode for phone view

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -32,24 +32,11 @@ const Navbar = () => {
             >
                 <Menu className='w-4 h-4' />
             </button>
-            <div className='relative'>
-                <input
-                    type='search'
-                    placeholder='Type to search products'
-                    className='pl-10 pr-4 py-2 w-50 md:w-60 border-2 border-grey-300 bg-white rounded-lg focus:outline-none focus:border-blue-500'
-                />
-                <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-                    <Bell 
-                        className='text-gray-500'
-                        size={20}
-                    />
-                </div>
-            </div>
         </div>
 
         {/* Right side */}
         <div className='flex justify-between items-center gap-5'>
-            <div className='hidden md:flex justify-between items-center gap-5'>
+            <div className='flex justify-between items-center gap-5'>
                 <div>
                     <button onClick={toggleDarkMode}>
                         {isDarkMode ? 
