4c003f40259a35de2cf98e2924c033797732f10f
rabeeh_rahman <rabeehrahman.rr@gmail.com>
2024-12-03 16:59:54 +0530

    adjusted button of transactions

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -88,17 +88,20 @@ const Transactions = () => {
       <div className="flex justify-between">
         <Header name="Transactions" />
 
-        <div className="flex justify-end items-center mb-6 gap-6">
+        <div className="flex flex-rowjustify-end items-center mb-6 gap-4 ml-5">
           <button 
             className="flex items-center bg-gray-900 hover:bg-orange-400 text-gray-200 font-bold py-2 px-4 rounded"
             onClick={() => setIsStoreOpen(true)}>
-            <PackagePlusIcon className="w-5 h-5 mr-2 !text-gray-200" /> Add to Store
+            <PackagePlusIcon className="w-5 h-5 mr-2 !text-gray-200" /> 
+            <span className="hidden sm:inline">Add to Store</span>
+            <span className="sm:hidden">Store</span>
           </button>
           <button
             className="flex items-center bg-gray-900 hover:bg-green-500 text-gray-200 font-bold py-2 px-4 rounded"
-            onClick={() => setIsShopOpen(true)}
-          >
-            <HousePlusIcon className="w-5 h-5 mr-2 !text-gray-200" /> Add to Shop
+            onClick={() => setIsShopOpen(true)}>
+            <HousePlusIcon className="w-5 h-5 mr-2 !text-gray-200" /> 
+            <span className="hidden sm:inline">Add to Shop</span>
+            <span className="sm:hidden">Shop</span>
           </button>
         </div>
       </div>
