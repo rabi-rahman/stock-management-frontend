e8fb8428d6b4089d85b734a0e1b3c1caf1640983
rabeeh_rahman <rabeehrahman.rr@gmail.com>
2024-12-18 22:49:12 +0530

    added eye to check password and enter key functionality in login

--- src/app/login/index.tsx
+++ src/app/login/index.tsx
@@ -1,42 +1,63 @@
 "use client";
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { Eye, EyeOff } from "lucide-react";
 
 export default function Login() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State for toggling password visibility
   const router = useRouter();
 
   const handleLogin = () => {
     const correctPassword = process.env.NEXT_PUBLIC_ADMIN_PASSWORD;
 
     if (password === correctPassword) {
-      // Store authentication status in localStorage
       localStorage.setItem("isAuthenticated", "true");
-      router.push("/dashboard"); // Navigate to the dashboard
+      router.push("/dashboard");
     } else {
       setError("Incorrect password. Please try again.");
     }
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    handleLogin();
   };
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <div className="p-5 bg-white rounded-lg shadow-lg w-full max-w-sm">
         <h1 className="text-2xl font-bold mb-5 text-center">Admin Login</h1>
-        <input
-          type="password"
-          placeholder="Enter Password"
-          className="w-full p-2 mb-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-slate-900"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        {error && <p className="text-red-500 mb-3 text-sm">{error}</p>}
-        <button
-          className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          onClick={handleLogin}
-        >
-          Login
-        </button>
+        <form onSubmit={handleSubmit}>
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              placeholder="Enter Password"
+              className="w-full p-2 mb-3 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500 text-slate-900"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button
+              type="button"
+              className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 hover:text-gray-700"
+              onClick={() => setShowPassword(!showPassword)} // Toggle showPassword state
+            >
+              {showPassword ? (
+                <Eye size={16} className="text-gray-200 mb-2" />
+              ) : (
+                <EyeOff size={16} className="text-gray-500 mb-2" />
+              )}
+            </button>
+          </div>
+          {error && <p className="text-red-500 mb-3 text-sm">{error}</p>}
+          <button
+            type="submit"
+            className="w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600"
+          >
+            Login
+          </button>
+        </form>
       </div>
     </div>
   );
